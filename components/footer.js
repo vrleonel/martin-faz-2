3cfeddda8f0636546ace48d2c4adb914492d1aab
Vitor Leonel <vitor.leonel@mercadolivre.com>
2022-06-11 15:49:28 -0300

    add assets and texts

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 export default function Footer({ children }) {
   return (
     <footer>
-      © 2022. Feito por Vitor &amp; Tati com carinho para o Martin
-      <span role="img" aria-label="heart">❤️</span>
-      <a href="https://vrleonel.dev">vrleone.dev</a>
+      <Image
+        src="/gaia-chapeuzinho.png"
+        width={212}
+        height={350}
+        alt="Gaia de chapéuzinho"
+      />
+      <p>
+        © 2022. Feito por Vitor &amp; Tati com carinho para o Martin{' '}
+        <span role="img" aria-label="heart">❤️</span>{' '}
+        <a href="https://vrleonel.dev">vrleone.dev</a>
+      </p>
     </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -11,33 +11,58 @@ export default function Home() {
         <meta name="description" content="Segunda volta ao sol do Martin" />
         <link rel="icon" href="/sol-tiny.png" />
       </Head>
-
+      
       <main className={styles.main}>
-        <section>
-          <Image alt="Martin e Gaia no carrinho" src="/martin-e-gaia.png" width="1054px" height="862px" />
-          <div>
-            <h2>Buffet Viva Vida</h2>
+        <section className="info">
+          <Image
+            alt="Martin e Gaia no carrinho"
+            src="/martin-e-gaia.png"
+            width="1054px"
+            height="862px" 
+            className="image"
+          />
+          <a href="https://goo.gl/maps/eXtTwN69zkQMEuA89" className="card">
+            <h2>21.08 - 12hr</h2>
+            <h3>Buffet Viva Vida</h3>
             <address>
-              Rua Corifeu de Azevedo Marques, xxx
+              Av. Corifeu de Azevedo Marques, 161
             </address>
-            <p>12hr</p>
-          </div>
+            
+          </a>
         </section>
-        <section>
+        <section className="donate">
           <h2>Presentes - Doações</h2>
+          <article>
+            <p>
+              Chegou a hora de comemorar os 2 anos do Martin e contamos com sua presença nesse momento.
+            </p>
+            <p>
+              Pedimos que o presente que você compraria para o Martin seja revertido em uma doação.
+            </p>
+            <p>
+              Juntos vamos celebrar a vida do Martin e ajudar duas instituições de amigos que precisam da sua ajuda para 
+              continuar ajudando famílias e pets.
+            </p>
+            <p>
+              As doações podem ser feitas via pix, comprando uma cesta básica no site ..... ou levando no dia do aniversário uma cesta básica ou ração.
+            </p>
+            <p>
+              Para conhecer mais o trabalho desses parceiros acesse os perfis no Instagram:
+            </p>
+          </article>
           <ul>
             <li>
-              <h3>Opção 1</h3>
-              <p>dados</p>
+              <h3>Projeto Corrente do Bem</h3>
+              <p><a href="https://www.instagram.com/projetocorrente_bem/" rel="noreferrer noopener" target="_blank">@projetocorrente_bem</a></p>
             </li>
             <li>
-              <h3>Opção 2</h3>
-              <p>dados</p>
+              <h3>Irmandade Animal</h3>
+              <p><a href="https://www.instagram.com/irmandadeanimal/" rel="noreferrer noopener" target="_blank">@irmandadeanimal</a></p>
             </li>
 
           </ul>
         </section>
-        <section>
+        <section className="form">
           <iframe
             src="https://docs.google.com/forms/d/e/1FAIpQLSez7PieTmNJkQK1pbPMxnNhEv5YGk-STBAF1yV1MFiktC-gVA/viewform?embedded=true" 
             width="640"
@@ -45,24 +70,12 @@ export default function Home() {
             frameBorder="0"
             marginHeight="0"
             marginWidth="0"
+            className="form-google"
           >
               Carregando…
             </iframe>
         </section>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
